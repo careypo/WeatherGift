37c97d12e8f722815ed81bab3e8e8a8e91e7b15e
Paige Carey <peace.paigey@gmail.com>
2018-10-22 19:06:18 -0400

    7.17 and icons complete

--- WeatherGift/DetailVC.swift
+++ WeatherGift/DetailVC.swift
@@ -44,6 +44,7 @@ class DetailVC: UIViewController {
         dateLabel.text = locationsArray[currentPage].coordinates
         temperatureLabel.text = locationsArray[currentPage].currentTemp
         summaryLabel.text = locationsArray[currentPage].currentSummary
+        currentImage.image = UIImage(named: locationsArray[currentPage].currentIcon)
 
     }
 }
--- WeatherGift/WeatherLocation.swift
+++ WeatherGift/WeatherLocation.swift
@@ -15,6 +15,7 @@ class WeatherLocation {
     var coordinates = ""
     var currentTemp = "--"
     var currentSummary = ""
+    var currentIcon = ""
     
     func getWeather(completed: @escaping () -> ()) {
         
@@ -34,6 +35,11 @@ class WeatherLocation {
                     self.currentSummary = summary
                 } else {
                     print("Could not return a summary")
+                }
+                if let icon  = json["currently"]["icon"].string {
+                    self.currentIcon = icon
+                } else {
+                    print("Could not return a icon")
                 }
             case .failure(let error):
                 print(error)
